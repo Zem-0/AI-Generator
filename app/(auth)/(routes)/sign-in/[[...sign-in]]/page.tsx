322c5dde7f9384da27e23e6caa02c2f67fd45e91
Parinith <parinith99@gmail.com>
2024-01-21 20:45:20 +0530

    first commit

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { SignIn } from "@clerk/nextjs";
+
+ function Page(){
+    return <SignIn/>
+}
+export default Page;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,14 @@
+import { LandingHero } from "@/components/LandingHero";
+import { LandingNavbar } from "@/components/landing-navbar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const LandingPage=()=>{
+    return(
+        <div className="h-full">
+            <LandingNavbar/>
+            <LandingHero/>
+        </div>
+    )
+}
+export default LandingPage;
